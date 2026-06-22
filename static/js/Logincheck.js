4d0d8b6125b82775712a38608b5b0ff7c1a5bd86
Suresh <suresheggogi@gmail.com>
2026-06-22 11:58:47 +0530

    trim

--- static/js/Logincheck.js
+++ static/js/Logincheck.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const homeButton = document.getElementById("homeButton");
-  const user = document.getElementById("unameInput").value.trim();
+  const user = document.getElementById("unameInput").value;
   const pass = document.getElementById("unamepwd").value;
 
   homeButton.addEventListener("click", function () {
