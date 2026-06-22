bad946fe3f89ebaaa44a40edcd481b613f65f4bd
Suresh <suresheggogi@gmail.com>
2026-06-22 12:23:20 +0530

    re enter

--- static/js/Logincheck.js
+++ static/js/Logincheck.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const homeButton = document.getElementById("homeButton");
-  const user = document.getElementById("unameInput").value;
-  const pass = document.getElementById("unamepwd").value;
 
   homeButton.addEventListener("click", function () {
-    if (user === "suresh" && pass === "suresh123")
-       {
+    const user = document.getElementById("unameInput").value;
+    const pass = document.getElementById("unamepwd").value;
+
+    if (user === "suresh" && pass === "suresh123") {
       window.location.href = "/home/";
-    } 
-    else {
+    } else {
       alert("Invalid credentials");
     }
   });
